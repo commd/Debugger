2c4dbf726e7e0ca9e0de6d2a6ea284e0674361bb
commd <vidscmd@netscape.net>
2016-10-04 03:52:26 +0000

    added problems and my solutions

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Debugger
 A simple html and javascript file created to allow you to use the Chrome debugger 
+
+* Write a function `squareAll` that takes an array of numbers as a parameter and returns a **new** array of the input numbers squared.
+* 
+* Write a function `validCredentials` that accepts two parameters: *username and *password*, and returns `true` if **both** are long enough, and `false' otherwise. You can decide what constitutes "long enough”.
+* 
+* Write a function `sayHello` that takes a *language* parameter, and returns "hello" in that language. Make the function work with **at least three languages**.
+* 
+Add Comment Collapse
--- writeJsHere.js
+++ writeJsHere.js
@@ -1,1 +1,42 @@
 //All your code goes here
+
+
+
+var squareAll = function (arr) {
+    var results = [];
+    results = arr.map(function(num) {
+        return num * num;
+    });
+    return results;
+}
+
+var ar = [1,3,5,9];
+console.log(ar);
+console.log(squareAll(ar));
+
+var validCredentials = function (username, password) {
+    var minLen = 8;
+    if (username.length < 8 || password.length < 8) {
+        return false;
+    } else return true;
+}
+
+var username = "JoeMahoney";
+var password = "SimplePassword";
+
+console.log (username);
+console.log (password);
+console.log (validCredentials(username, password));
+password = "short";
+console.log (password);
+console.log (validCredentials(username, password));
+
+var sayHello = function (lang) {
+    if (lang == "en") return ("Hello!");
+    if (lang == 'es') return ('Hola!');
+    if (lang == 'fr') return ('Bonjour!');
+    return ("Sorry, don't know that language.");
+}
+
+console.log (sayHello('es'));
+console.log (sayHello('ch'));
